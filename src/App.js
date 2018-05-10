02fe1c5e51719a4f21f61d0653fc43a579196f80
Rana Faiz Ahmad <iamfaizahmed123@gmail.com>
2018-05-10 09:46:33 -0500

    Add a basename for the router

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router basename="/react-auth-ui/">
         <div className="App">
           <div className="App__Aside"></div>
           <div className="App__Form">
